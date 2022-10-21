6e04264987a9c413078859c5ed63c030f7c6fbc9
Novan Agung Waskito <68317534+jaygung17@users.noreply.github.com>
2022-10-21 13:50:04 +0700

    Done tugasnya mas azam, boleh coba di code review mas azam

--- StoryboardDesign/LoginController.swift
+++ StoryboardDesign/LoginController.swift
@@ -8,8 +8,69 @@
 import Foundation
 import UIKit
 
-let MyUsername = "agung.luo"
-let MyPassword = "qwertyuiop"
+class LoginController: UIViewController {
+    //properties for part of login page
+    @IBOutlet var UsernameTextField: UITextField!
+    @IBOutlet var PasswordTextField: UITextField!
+    let loggedInUserDefaultsKey = "com.funios.loggedInkey"
+    
+    
+    @IBAction func VerifyMethod(_ sender: Any)
+    {
+        //Credential Data for login
+        let MyUsername = "a"
+        let MyPassword = "a"
+        
+        //Verified with if else method
+        if UsernameTextField.text == MyUsername && PasswordTextField.text == MyPassword {
+            saveIsUserLoggeedIn(userLoginSuccesfully: true)
+            saveInputtedUsername(inputedUsername: MyUsername)
+            perfomSegueToMovieList ()
+        } else {
+            let alert = UIAlertController(
+                title: "Invalid Login",
+                message: "Please fill username and password correctly",
+                preferredStyle: UIAlertController.Style.alert)
+            
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                // do something when user press OK button, like deleting text in both fields or do nothing
+            }
+            
+            alert.addAction(OKAction)
+            present(alert, animated: true, completion: nil)
+            return
+        }
+    }
+    
+   override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.isHidden = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        isUserAlreadyLoggedIn()
+        view.isHidden = false
+    }
+    func isUserAlreadyLoggedIn() {
+        if UserDefaults.standard.bool(forKey: loggedInUserDefaultsKey) {
+           perfomSegueToMovieList()
+        }
+    }
+    
 
-viewDidLoad()
-
+    func perfomSegueToMovieList() {
+        performSegue(withIdentifier: "VerifiedSegue", sender: nil)
+    }
+    func saveIsUserLoggeedIn(userLoginSuccesfully isLogin: Bool) {
+        AuthSingleton.instance.saveUserLogin(isLogin: isLogin)
+    }
+    
+    func saveInputtedUsername(inputedUsername username: String) {
+        let usernameUserdefaultsKey = "com.funios.usernameKey"
+        AuthSingleton.instance.saveUserName(name: username)
+    }
+}
